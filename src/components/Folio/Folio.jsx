f2e023064f9da850eae36299e0242973c5070873
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-02-17 15:15:43 -0700

    Set up usedarkmode function

--- src/components/Folio/Folio.jsx
+++ src/components/Folio/Folio.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from "../useDarkMode";
+import { lightTheme, darkTheme } from '../theme';
+import { GlobalStyles } from '../../global';
 import "./Folio.css";
 import Navbar from "../Navbar/Navbar";
 import ThemePanel from "../ThemePanel/ThemePanel";
 
 const Folio = () => {
     const [display, setDisplay] = useState("Home");
-    const [theme, setTheme] = useState("light");
+    const [theme, toggleTheme] = useDarkMode();
+    const themeMode = theme === "light" ? lightTheme : darkTheme;
 
     const updateDisplay = (page) => { setDisplay(page) };
-    const updateTheme = (theme) => { setTheme(theme) };
-
+    
     return(
-        <div id="Folio">
-            <Navbar update={updateDisplay}/>
-            <ThemePanel theme={theme} update={updateTheme}/>
-        </div>
+        <ThemeProvider theme={themeMode}>
+            <>
+                <GlobalStyles />
+                <div id="Folio">
+                    <Navbar update={updateDisplay}/>
+                    <ThemePanel theme={theme} update={toggleTheme}/>
+                </div>
+            </>    
+        </ThemeProvider>
+        
     );
 }
 
